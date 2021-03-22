fe5a019ab21ba2fab69127895ab2a664fe79845d
JobCollins <dulo.collins@gmail.com>
2021-03-22 13:25:32 +0300

    Feature: Rating Component
    ratings in stars read from the the array in product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
 
 export default class Product extends Component {
     render() {
+        const { _id, image, name, rating, numReviews, price } = this.props.product
         return (
             <Card className='my-3 p-3 rounded'>
-                <a href={`.product/${this.props.product._id}`}>
-                    <Card.Img src={this.props.product.image} variant='top' />
+                <a href={`.product/${_id}`}>
+                    <Card.Img src={image} variant='top' />
                 </a>
                 <Card.Body>
-                <a href={`.product/${this.props.product._id}`}>
-                    <Card.Title as='div'><strong>{this.props.product.name}</strong></Card.Title>
+                <a href={`.product/${_id}`}>
+                    <Card.Title as='div'><strong>{name}</strong></Card.Title>
                 </a>
                 <Card.Text as='div'>
-                    <div className='my-3'>
-                        {this.props.product.rating} from {this.props.product.numReviews}
-                    </div>
+                    <Rating value={rating} text={`${numReviews} reviews`} />
                 </Card.Text>
                 <Card.Text as='h3'>
-                    ${this.props.product.price}
+                    ${price}
                 </Card.Text>
                 </Card.Body>
             </Card>
