ce4ec1a637b10895ceeb7b4e295aaccba537a4e7
JobCollins <dulo.collins@gmail.com>
2021-03-22 12:28:10 +0300

    Feature: FrontEnd Header
    build of navbar and navlinks together with icons for signin and cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <>
-      <h1>Karibu dukani.</h1>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Karibu dukani</h1>
+        </Container>
+      </main>
+      <Footer/>
+      </>
+    )
+  }
 }
 
-export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, Container  } from 'react-bootstrap'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <Navbar bg="light" expand="lg" collapseOnSelect>
+                    <Container>
+                        <Navbar.Brand href="#home">dukani</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto">
+                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                    
+                </Navbar>
+            </header>
+        )
+    }
+}
