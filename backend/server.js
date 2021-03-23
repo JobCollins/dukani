994f608e000ceb5810f483a2ef84a205e4366bbb
JobCollins <dulo.collins@gmail.com>
2021-03-23 20:58:45 +0300

    Feature: Custom error handling
    handling our error in a custom way and testing on postman

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-
+import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -17,6 +17,12 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+//fallback for 404 errors
+app.use(notFound)
+
+//error middleware to override default error handler
+app.use(errorHandler)
 
 const port = process.env.PORT || 5000
 
