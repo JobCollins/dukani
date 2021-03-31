42426ae853cf51ae0824842146dfba5107f4fa84
JobCollins <dulo.collins@gmail.com>
2021-03-31 13:33:36 +0300

    Features: Cart
    implementation of add to cart functionality

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,7 +4,7 @@ import {CART_ADD_ITEM} from '../constants/cartConstants'
 export const cartReducers = (state={ cartItems: []}, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
-            const item = action.payload()
+            const item = action.payload
 
             const existItem = state.cartItems.find(
                 x => x.product === item.product
