92d0c0cfba29febe65406f987d489ce9d7a25d12
JobCollins <dulo.collins@gmail.com>
2021-03-31 12:55:32 +0300

    Feature: Product Quantity
    application allows selection of quantity according to number of items in stock.
    started on the Cart screen build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import React, { Component } from 'react'
@@ -16,6 +17,7 @@ export default class App extends Component {
           <Container>
             <Route path='/' component={HomeScreen} exact/>
             <Route path='/product/:id' component={ProductScreen}/>
+            <Route path='/cart/:id?' component={CartScreen}/>
           </Container>
         </main>
         <Footer/>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Button, Card, Col, Image, ListGroup, ListGroupItem, Row, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux'
 import Rating from '../components/Rating'
@@ -7,7 +7,9 @@ import {listProductDetails} from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-const ProductScreen = ({match}) =>  {
+const ProductScreen = ({history, match}) =>  {
+
+    const [qty, setQty] = useState(0)
 
     const dispatch = useDispatch()
 
@@ -19,6 +21,9 @@ const ProductScreen = ({match}) =>  {
     }, [dispatch, match])
 
     
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
     
     // const { match } = this.props
     // const product = product.find((p) => p._id === match.params.id)
@@ -77,8 +82,35 @@ const ProductScreen = ({match}) =>  {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+                            {
+                                product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Qty: </Col>
+                                            <Col>
+                                            <Form.Control
+                                            as='select'
+                                            value={qty}
+                                            onChange={(e) => setQty(e.target.value)}
+                                            >
+                                                {
+                                                    [...Array(product.countInStock).keys()].map(
+                                                        (x) => (
+                                                            <option key={x+1} value={x+1}>
+                                                                {x+1}
+                                                            </option>
+                                                        )
+                                                    )
+                                                }
+                                            </Form.Control> 
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                )
+                            }
+                            
                             <ListGroup.Item>
-                                <Button className='btn-black' type='button' disabled={product.countInStock === 0}>
+                                <Button onClick={addToCartHandler} className='btn-black' type='button' disabled={product.countInStock === 0}>
                                     Add to Cart
                                 </Button>
                             </ListGroup.Item>
