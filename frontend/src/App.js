fae50bc7d49f76d772b82777727f5e9114d096c5
JobCollins <dulo.collins@gmail.com>
2021-04-06 17:21:19 +0300

    Feature: Show User in Navbar & Logout
    navbar updates from sign in to user name.. User can now sign in and sign out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/Header'
+import {Header} from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
