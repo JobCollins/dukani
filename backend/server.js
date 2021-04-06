7fbca3014dc18ed2d13912047967652f9e71fbd3
JobCollins <dulo.collins@gmail.com>
2021-04-06 15:26:59 +0300

    Feature: User Authentication
    built the user authentication endpoint and tested it on postman.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 dotenv.config()
@@ -12,11 +13,15 @@ connectDB()
 
 const app = express()
 
+//to acccept json in request body
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //fallback for 404 errors
 app.use(notFound)
