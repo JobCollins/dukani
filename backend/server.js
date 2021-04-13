5b2b6be3f9d6cbc83515c2a855f6ab888ec57a74
JobCollins <dulo.collins@gmail.com>
2021-04-13 17:02:32 +0300

    Bug: Failing to add Paypal Payments
    the PUT method returns an Internal Server Error

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,8 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
 //fallback for 404 errors
 app.use(notFound)
 
